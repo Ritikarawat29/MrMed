ec7060d9be35d1d028259fd3ae8259e01dc61016
Ritika <rrawat45@r1rcm.com>
2022-11-01 15:09:14 +0530

    first commit

---
+++ MrMed/Controllers/AppointmentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MrMed.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: Appointment
+        public ActionResult Appointment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Appointment(Appointment obj)
+
+
+
+        {
+            if (ModelState.IsValid)
+            {
+                RitikaTestEntities db = new RitikaTestEntities();
+                db.Appointments.Add(obj);
+             
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+    }
+}
